c0a65717d179264bccb5312b097dcd65d3d0eda1
manuela.rodriguez <manuela.rodriguez@dataprev.gov.br>
2021-12-07 01:31:22 -0300

    Fixing sair bug

--- src/layout/Base.vue
+++ src/layout/Base.vue
@@ -2,13 +2,13 @@
   <div>
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark fixed-top pl-3 pt-0 pb-0">
-        <img
-          src="https://i.imgur.com/jGIvAzn.png"
-          width="170px"
-          />
-        <router-link class="navbar-brand ml-2" to="/">Organo</router-link>
+	<img	
+          src="https://i.imgur.com/jGIvAzn.png"	
+          width="170px"	
+         />
+	<router-link class="navbar-brand ml-2" to="/">Organo</router-link>
         <div class="ml-auto">
-          <div class="btn btn-primary my-2 my-sm-0" v-if="Object.keys(comprador).length !== 0">
+          <div class="btn btn-primary my-2 my-sm-0" v-if="Object.keys(comprador).length !== 0 && Object.keys(fornecedor).length === 0">
           <router-link class="btn btn-primary my-2 my-sm-0" to="/comprador">
             <img
               src="https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/8-512.png"
@@ -17,7 +17,16 @@
             />
             </router-link>
           </div>
-          <router-link v-else class="btn btn-primary my-2 my-sm-0 navbar-option" to="/loginComprador">Login</router-link>
+          <div class="btn btn-primary my-2 my-sm-0" v-if="Object.keys(comprador).length === 0 && Object.keys(fornecedor).length !== 0">
+            <router-link class="btn btn-primary my-2 my-sm-0" to="/fornecedor">
+              <img
+                src="https://cdn0.iconfinder.com/data/icons/set-ui-app-android/32/8-512.png"
+                class="img-thumbnail profile-image"
+                alt
+              />
+            </router-link>
+          </div>
+          <router-link v-else-if="Object.keys(comprador).length === 0 && Object.keys(fornecedor).length === 0" class="btn btn-primary my-2 my-sm-0 navbar-option" to="/loginComprador">Login</router-link>
           <router-link class="btn btn-primary my-2 my-sm-0" to="/carrinho">
               <img
                 src="https://i.imgur.com/lXAH3Yy.png"
@@ -26,7 +35,7 @@
               />
               <span class="badge badge-danger badge-pill">{{ carrinho.length }}</span>
           </router-link>
-          <router-link class="btn btn-primary my-2 my-sm-0 navbar-option" to="/loginFornecedor"> Sou fornecedor
+          <router-link class="btn btn-primary my-2 my-sm-0 navbar-option" v-if="Object.keys(fornecedor).length === 0" to="/loginFornecedor"> Sou fornecedor
           </router-link>
           <button v-if="(comprador && Object.keys(comprador).length !== 0) || (fornecedor && Object.keys(fornecedor).length !== 0)"
           @click="encerrarSessao()"
@@ -52,9 +61,9 @@ export default {
   methods: {
     ...mapActions("usuario", ["sair"]),
     encerrarSessao() {
-      if(this.comprador) {
+      if(Object.keys(this.comprador).length !== 0) {
         this.sair({ id: this.comprador.id, type: 'comprador'} )
-      } else if(this.fornecedor) {
+      } else if(Object.keys(this.fornecedor).length !== 0) {
         this.sair({ id: this.fornecedor.id, type: 'fornecedor'})
       }   
     }
@@ -67,30 +76,26 @@ export default {
 nav {
   background-color: #6aa84f;
 }
-
 .navbar-brand {
-  font-weight: 500;
-  font-size: 50px;
-  color: #ffffff !important;
-  font-family: 'Poiret One', cursive;
-  margin-top: -0.15em;
-  margin-bottom: -0.05em;
+  font-weight: 500;	
+  font-size: 50px;	
+  color: #ffffff !important;	
+  font-family: 'Poiret One', cursive;	
+  margin-top: -0.15em;	
+  margin-bottom: -0.05em;	
   padding: 0;
 }
 
-.navbar-option {
-  font-size: 20px;
+.navbar-option {	
+  font-size: 20px;	
 }
-
 .profile-image {
   width: 50px;
   border-radius: 100% !important;
 }
-
 .page-container {
   padding-top: 81px;
 }
-
 .btn {
   border-radius: 0%;
   font-weight: bold;
