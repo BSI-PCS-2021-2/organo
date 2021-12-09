fabe3b3a605fb63c8321a12e1ef8ca19f39592c4
manuela.rodriguez <manuela.rodriguez@dataprev.gov.br>
2021-12-08 22:29:58 -0300

    Fixing missing console.log

--- src/organo-store/usuario/actions.js
+++ src/organo-store/usuario/actions.js
@@ -50,7 +50,7 @@ export function login({ commit }, user) {
                 icon: 'error',
                 confirmButtonText: 'Ok'
               })
-            (error)
+            console.log(error)
         });
 }
 
